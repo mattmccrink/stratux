4ff773cb7a0017b2b85ba5d91cc19c89f21a826d
mattmccrink <mattmccrink@gmail.com>
2018-07-30 17:20:30 +0000

    Added installation script

---
+++ stratux_shell.sh
@@ -0,0 +1,395 @@
+# Copyright (c) 2016 Joseph D Poirier
+# Distributable under the terms of The New BSD License
+# that can be found in the LICENSE file.
+
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+
+if [ $(whoami) != 'root' ]; then
+    echo "${BOLD}${RED}This script must be executed as root, exiting...${WHITE}${NORMAL}"
+    exit
+fi
+
+
+SCRIPTDIR="`pwd`"
+
+#set -e
+
+#outfile=setuplog
+#rm -f $outfile
+
+#exec > >(cat >> $outfile)
+#exec 2> >(cat >> $outfile)
+
+#### stdout and stderr to log file
+#exec > >(tee -a $outfile >&1)
+#exec 2> >(tee -a $outfile >&2)
+
+#### execute the script: bash stratux-setup.sh
+
+#### Revision numbers found via cat /proc/cpuinfo
+# [Labeled Section]                                       [File]
+# Dependencies                                          - stratux-setup.sh
+# Hardware check                                        - stratux-setup.sh
+# Setup /etc/hostapd/hostapd.conf                       - wifi-ap.sh
+# Edimax WiFi check                                     - stratux-wifi.sh
+# Boot config settings                                  - rpi.sh
+# RPi 0/2 check to enable Edimax wifi dongle option     - rpi.sh
+#
+RPI0xREV=900092
+RPI0yREV=900093
+
+RPI2BxREV=a01041
+RPI2ByREV=a21041
+
+RPI3BxREV=a02082
+RPI3ByREV=a22082
+
+ODROIDC2=020b
+
+CHIP=0000
+
+#### unchecked
+RPIBPxREV=0010
+RPIAPxREV=0012
+RPIBPyREV=0013
+
+REVISION="$(cat /proc/cpuinfo | grep Revision | cut -d ':' -f 2 | xargs)"
+
+
+# Processor 
+# [Labeled Section]                                       [File]
+# Go bootstrap compiler installation                    - stratux-setup.sh
+#
+ARM6L=armv6l
+ARM7L=armv7l
+ARM64=aarch64
+
+MACHINE="$(uname -m)"
+
+# Edimax WiFi dongle
+EW7811Un=$(lsusb | grep EW-7811Un)
+
+
+echo "${MAGENTA}"
+echo "************************************"
+echo "**** Stratux Setup Starting... *****"
+echo "************************************"
+echo "${WHITE}"
+
+if which ntp >/dev/null; then
+    ntp -q -g
+fi
+
+
+##############################################################
+##  Stop exisiting services
+##############################################################
+echo
+echo "${YELLOW}**** Stop exisiting services... *****${WHITE}"
+
+service stratux stop
+echo "${MAGENTA}stratux service stopped...${WHITE}"
+
+if [ -f "/etc/init.d/stratux" ]; then
+    # remove old file
+    rm -f /etc/init.d/stratux
+    echo "/etc/init.d/stratux file found and deleted...${WHITE}"
+fi
+
+if [ -f "/etc/init.d/hostapd" ]; then
+    service hostapd stop
+    echo "${MAGENTA}hostapd service found and stopped...${WHITE}"
+fi
+
+if [ -f "/etc/init.d/isc-dhcp-server" ]; then
+    service isc-dhcp-server stop
+    echo "${MAGENTA}isc-dhcp service found and stopped...${WHITE}"
+fi
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Dependencies
+##############################################################
+echo
+echo "${YELLOW}**** Installing dependencies... *****${WHITE}"
+
+if [ "$REVISION" == "$RPI2BxREV" ] || [ "$REVISION" == "$RPI2ByREV" ]  || [ "$REVISION" == "$RPI3BxREV" ] || [ "$REVISION" == "$RPI3ByREV" ] || [ "$REVISION" == "$RPI0xREV" ] || [ "$REVISION" == "$RPI0yREV" ]; then
+    apt-get install -y rpi-update
+    rpi-update
+fi
+
+apt-get update
+apt-mark hold plymouth
+apt-get dist-upgrade -y
+apt-get upgrade -y
+apt-get install -y git
+git config --global http.sslVerify false
+apt-get install -y iw
+apt-get install -y lshw
+apt-get install -y wget
+apt-get install -y isc-dhcp-server
+apt-get install -y tcpdump
+apt-get install -y cmake
+apt-get install -y libusb-1.0-0.dev
+apt-get install -y build-essential
+apt-get install -y mercurial
+apt-get install -y autoconf
+apt-get install -y fftw3
+apt-get install -y fftw3-dev
+apt-get install -y libtool
+apt-get install -y automake
+apt-get remove -y hostapd
+apt-get install -y hostapd
+apt-get install -y pkg-config
+apt-get install -y libjpeg-dev i2c-tools python-smbus python-pip python-dev python-pil python-daemon screen
+#apt-get purge golang*
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Hardware check
+##############################################################
+echo
+echo "${YELLOW}**** Hardware check... *****${WHITE}"
+
+if [ "$REVISION" == "$RPI2BxREV" ] || [ "$REVISION" == "$RPI2ByREV" ]  || [ "$REVISION" == "$RPI3BxREV" ] || [ "$REVISION" == "$RPI3ByREV" ] || [ "$REVISION" == "$RPI0xREV" ] || [ "$REVISION" == "$RPI0yREV" ]; then
+    echo
+    echo "${MAGENTA}Raspberry Pi detected...${WHITE}"
+
+    . ${SCRIPTDIR}/rpi.sh
+elif [ "$REVISION" == "$ODROIDC2" ]; then
+    echo
+    echo "${MAGENTA}Odroid-C2 detected...${WHITE}"
+
+    . ${SCRIPTDIR}/odroid.sh
+elif [ "$REVISION" == "$CHIP" ]; then
+    echo
+    echo "${MAGENTA}CHIP detected...${WHITE}"
+
+    . ${SCRIPTDIR}/chip.sh
+else
+    echo
+    echo "${BOLD}${RED}WARNING - unable to identify the board using /proc/cpuinfo...${WHITE}${NORMAL}"
+
+    #exit
+fi
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Stratux USB devices udev rules
+##############################################################
+echo
+echo "${YELLOW}**** Stratux USB devices udev rules to /etc/udev/rules.d/10-stratux.rules *****${WHITE}"
+
+cat <<EOT > /etc/udev/rules.d/10-stratux.rules
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a8", SYMLINK+="ublox8"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a7", SYMLINK+="ublox7"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a6", SYMLINK+="ublox6"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", SYMLINK+="prolific%n"
+SUBSYSTEMS=="usb", ATTRS{interface}=="Stratux Serialout", SYMLINK+="serialout%n"
+EOT
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  SSH setup and config
+##############################################################
+echo
+echo "${YELLOW}**** SSH setup and config... *****${WHITE}"
+
+if [ ! -d /etc/ssh/authorized_keys ]; then
+    mkdir -p /etc/ssh/authorized_keys
+fi
+
+cp -n /etc/ssh/authorized_keys/root{,.bak}
+cp -f ${SCRIPTDIR}/files/root /etc/ssh/authorized_keys/root
+chown root.root /etc/ssh/authorized_keys/root
+chmod 644 /etc/ssh/authorized_keys/root
+
+cp -n /etc/ssh/sshd_config{,.bak}
+cp -f ${SCRIPTDIR}/files/sshd_config /etc/ssh/sshd_config
+rm -f /usr/share/dbus-1/system-services/fi.epitest.hostap.WPASupplicant.service
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Hardware blacklisting
+##############################################################
+echo
+echo "${YELLOW}**** Hardware blacklisting... *****${WHITE}"
+
+if ! grep -q "blacklist dvb_usb_rtl28xxu" "/etc/modprobe.d/rtl-sdr-blacklist.conf"; then
+    echo blacklist dvb_usb_rtl28xxu >>/etc/modprobe.d/rtl-sdr-blacklist.conf
+fi
+
+if ! grep -q "blacklist e4000" "/etc/modprobe.d/rtl-sdr-blacklist.conf"; then
+    echo blacklist e4000 >>/etc/modprobe.d/rtl-sdr-blacklist.conf
+fi
+
+if ! grep -q "blacklist rtl2832" "/etc/modprobe.d/rtl-sdr-blacklist.conf"; then
+    echo blacklist rtl2832 >>/etc/modprobe.d/rtl-sdr-blacklist.conf
+fi
+
+if ! grep -q "blacklist dvb_usb_rtl2832u" "/etc/modprobe.d/rtl-sdr-blacklist.conf"; then
+    echo blacklist dvb_usb_rtl2832u >>/etc/modprobe.d/rtl-sdr-blacklist.conf
+fi
+
+
+##############################################################
+##  Go environment setup
+##############################################################
+echo
+echo "${YELLOW}**** Go environment setup... *****${WHITE}"
+
+# if any of the following environment variables are set in .bashrc delete them
+if grep -q "export GOROOT_BOOTSTRAP=" "/root/.bashrc"; then
+    line=$(grep -n 'GOROOT_BOOTSTRAP=' /root/.bashrc | awk -F':' '{print $1}')d
+    sed -i $line /root/.bashrc
+fi
+
+if grep -q "export GOPATH=" "/root/.bashrc"; then
+    line=$(grep -n 'GOPATH=' /root/.bashrc | awk -F':' '{print $1}')d
+    sed -i $line /root/.bashrc
+fi
+
+if grep -q "export GOROOT=" "/root/.bashrc"; then
+    line=$(grep -n 'GOROOT=' /root/.bashrc | awk -F':' '{print $1}')d
+    sed -i $line /root/.bashrc
+fi
+
+if grep -q "export PATH=" "/root/.bashrc"; then
+    line=$(grep -n 'PATH=' /root/.bashrc | awk -F':' '{print $1}')d
+    sed -i $line /root/.bashrc
+fi
+
+# only add new paths
+XPATH="\$PATH"
+if [[ ! "$PATH" =~ "/root/go/bin" ]]; then
+    XPATH+=:/root/go/bin
+fi
+
+if [[ ! "$PATH" =~ "/root/go_path/bin" ]]; then
+    XPATH+=:/root/go_path/bin
+fi
+
+echo export GOROOT_BOOTSTRAP=/root/gobootstrap >>/root/.bashrc
+echo export GOPATH=/root/go_path >>/root/.bashrc
+echo export GOROOT=/root/go >>/root/.bashrc
+echo export PATH=${XPATH} >>/root/.bashrc
+
+export GOROOT_BOOTSTRAP=/root/gobootstrap
+export GOPATH=/root/go_path
+export GOROOT=/root/go
+export PATH=${PATH}:/root/go/bin:/root/go_path/bin
+
+source /root/.bashrc
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  Go bootstrap compiler installation
+##############################################################
+echo
+echo "${YELLOW}**** Go bootstrap compiler installtion... *****${WHITE}"
+
+cd /root
+
+rm -rf go/
+rm -rf gobootstrap/
+
+if [ "$MACHINE" == "$ARM6L" ] || [ "$MACHINE" == "$ARM7L" ]; then
+    #### For RPi-2/3, is there any disadvantage to using the armv6l compiler?
+    wget https://storage.googleapis.com/golang/go1.10.3.linux-armv6l.tar.gz --no-check-certificate
+    tar -zxvf go1.10.3.linux-armv6l.tar.gz
+
+    if [ ! -d /root/go ]; then
+        echo "${BOLD}${RED}ERROR - go folder doesn't exist, exiting...${WHITE}${NORMAL}"
+        exit
+    fi
+
+#    if [ "$MACHINE" == "$ARM6L" ]; then
+#        export GOARM=6
+#    else
+#        export GOARM=7
+#    then
+elif [ "$MACHINE" == "$ARM64" ]; then
+    # ulimit -s 1024     # set the thread stack limit to 1mb
+    # ulimit -s          # check that it worked
+    # env GO_TEST_TIMEOUT_SCALE=10 GOROOT_BOOTSTRAP=/root/gobootstrap
+
+    wget https://github.com/jpoirier/GoAarch64Binaries/raw/master/go1.6.linux-armvAarch64.tar.gz --no-check-certificate
+    tar -zxvf go1.6.linux-armvAarch64.tar.gz
+    if [ ! -d /root/go ]; then
+        echo "${BOLD}${RED}ERROR - go folder doesn't exist, exiting...${WHITE}${NORMAL}"
+        exit
+    fi
+else
+    echo
+    echo "${BOLD}${RED}ERROR - unsupported machine type: $MACHINE, exiting...${WHITE}${NORMAL}"
+fi
+
+rm -f go1.*.linux*
+rm -rf /root/go_path
+mkdir -p /root/go_path
+
+echo "${GREEN}...done${WHITE}"
+
+
+##############################################################
+##  RTL-SDR tools build
+##############################################################
+echo
+echo "${YELLOW}**** RTL-SDR library build... *****${WHITE}"
+
+cd /root
+
+rm -rf librtlsdr
+git clone https://github.com/jpoirier/librtlsdr
+cd librtlsdr
+mkdir build
+cd build
+cmake ../
+make
+make install
+ldconfig
+
+echo "${GREEN}...done${WHITE}"
+
+##############################################################
+##  WiringPi tools build
+##############################################################
+
+echo
+echo "${YELLOW}**** WiringPi Install ****${WHITE}"
+cd /tmp
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+echo "${GREEN}...done${WHITE}"
+
+
+
